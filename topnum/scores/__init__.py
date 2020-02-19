67770134cab6add9dd27f0acaac809262e33f713
Vasiliy Alekseev <alvasian@yandex.ru>
2020-02-19 19:08:10 +0300

    add todo about two versions

--- topnum/scores/__init__.py
+++ topnum/scores/__init__.py
@@ -1,5 +1,7 @@
 from .entropy_score import EntropyScore
 from .intratext_coherence_score import IntratextCoherenceScore
 from .perplexity_score import PerplexityScore
+
+# TODO: find out which implementation is better and probably keep just one
 from .simple_toptok_coherence_score import SimpleTopTokensCoherenceScore
 from .sophisticated_toptok_coherence_score import SophisticatedTopTokensCoherenceScore
