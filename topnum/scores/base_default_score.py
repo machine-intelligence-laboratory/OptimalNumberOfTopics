869cb238bcf306adeeea9209694a30a528d0d497
Vasiliy Alekseev <alvasian@yandex.ru>
2020-07-04 17:07:00 +0300

    add deepcopy in base default score

--- topnum/scores/base_default_score.py
+++ topnum/scores/base_default_score.py
@@ -1,3 +1,5 @@
+import copy
+
 from artm.scores import BaseScore as BaseArtmScore
 from topicnet.cooking_machine.models import TopicModel
 
@@ -14,7 +16,7 @@ class BaseDefaultScore(BaseScore):
         # TODO: workaround: maybe a better way is possible
         setattr(self._score, '_higher_better', self._higher_better)
 
-        model._model.scores.add(self._score)
+        model._model.scores.add(copy.deepcopy(self._score))
 
     def _initialize(self) -> BaseArtmScore:
         # return artm.scores.<SCORE>(...)
